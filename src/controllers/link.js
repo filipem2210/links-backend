74638acb04edfeeca82941d9ef3cfc44ca0d8ef3
Filipe Moraes <filipemoraes2210@gmail.com>
2020-07-01 17:02:39 -0300

    Verify JWT

--- src/controllers/link.js
+++ src/controllers/link.js
@@ -5,14 +5,14 @@ const { Link } = require('../models');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-  const accountId = 1; //req.id;
+  const { accountId } = req;
   const links = await Link.findAll({ where: { accountId } });
 
   return res.jsonOK(links);
 });
 
 router.get('/:id', async (req, res) => {
-  const accountId = 1; //req.id;
+  const { accountId } = req;
   const { id } = req.params;
   const link = await Link.findOne({ where: { id, accountId } });
   if (!link) return res.jsonNotFound();
@@ -21,8 +21,8 @@ router.get('/:id', async (req, res) => {
 });
 
 router.post('/', async (req, res) => {
-  const accountId = 2; //req.id;
-  const { label, url, isSocial } = req.body;
+  const { accountId, body } = req;
+  const { label, url, isSocial } = body;
 
   const image = 'https://google.com/image.jpg'
 
@@ -32,9 +32,8 @@ router.post('/', async (req, res) => {
 });
 
 router.put('/:id', async (req, res) => {
-  const accountId = 1; //req.id;
+  const { accountId, body } = req;
   const { id } = req.params;
-  const { body } = req;
   const fields = ['label', 'url', 'isSocial'];
 
   const link = await Link.findOne({ where: { id, accountId } });
@@ -51,7 +50,7 @@ router.put('/:id', async (req, res) => {
 });
 
 router.delete('/:id', async (req, res) => {
-  const accountId = 2; //req.id;
+  const { accountId } = req;
   const { id } = req.params;
   const link = await Link.findOne({ where: { id, accountId } });
   if (!link) return res.jsonNotFound();
